ee7a303a7f2e6faf7ddc67886ce8c47fda5e09e4
Codemis <johnathan@still-water.com>
2012-05-11 13:21:52 -0700

    Added customization through the url.

--- upgotd_customizer.php
+++ upgotd_customizer.php
@@ -20,31 +20,84 @@
  * @copyright Copyright 2012 Missional Digerati
  */
 /**
- * Set the Center Font, Link, & Link Hover Color
+ * Set the Center Background, Font, Link, & Link Hover Color
  *
  * @author Johnathan Pulos
  */
+$center_bg_color = (isset($_GET['cbg'])) ? '#'.$_GET['cbg'] : '#000000';
 $center_font_color = (isset($_GET['cfc'])) ? '#'.$_GET['cfc'] : '#000000';
 $center_link_color = (isset($_GET['clc'])) ? '#'.$_GET['clc'] : '#0000FF';
 $center_hover_color = (isset($_GET['chc'])) ? '#'.$_GET['chc'] : '#0000FF';
+/**
+ * Set the Banner Background, Link, & Link Hover Color
+ *
+ * @author Johnathan Pulos
+ */
+$banner_bg_color = (isset($_GET['bbg'])) ? '#'.$_GET['bbg'] : '#BBDDFF';
+$banner_link_color = (isset($_GET['blc'])) ? '#'.$_GET['blc'] : '#0000FF';
+$banner_hover_color = (isset($_GET['bhc'])) ? '#'.$_GET['bhc'] : '#0000FF';
+/**
+ * Set the Footer Background, Font, Link, & Link Hover Color
+ *
+ * @author Johnathan Pulos
+ */
+$footer_bg_color = (isset($_GET['fbg'])) ? '#'.$_GET['fbg'] : '#BBDDFF';
+$footer_font_color = (isset($_GET['ffc'])) ? '#'.$_GET['ffc'] : '#000000';
+$footer_link_color = (isset($_GET['flc'])) ? '#'.$_GET['flc'] : '#0000FF';
+$footer_hover_color = (isset($_GET['fhc'])) ? '#'.$_GET['fhc'] : '#0000FF';
+/**
+ * Set the widget border details
+ *
+ * @author Johnathan Pulos
+ */
+$border = '';
+$has_border = (isset($_GET['nb'])) ? $_GET['nb'] : 'true';
+if($has_border == 'true') {
+	$border_type = (isset($_GET['bdt'])) ? $_GET['bdt'] : 'dashed';
+	$border_color = (isset($_GET['bdc'])) ? '#'.$_GET['bdc'] : '#CCCCCC';
+	$border_width = (isset($_GET['bdw'])) ? $_GET['bdw'] : '1px';
+	$border = 'border: ' . $border_color . ' ' . $border_width . ' ' . $border_type. ';';
+}
+/**
+ * Set the final language
+ *
+ * @todo We need to fix international upgotd_custom.php before using lang
+ * @author Johnathan Pulos
+ */
+$lang = (isset($_GET['lang'])) ? $_GET['lang'] : '';
+$custom_js = ($lang == '') ? 'http://www.joshuaproject.net/upgotd_custom.php' : 'http://www.joshuaproject.net/international/'.$lang.'/upgotd_custom.php';
+/**
+ * Handle narrow widget
+ *
+ * @author Johnathan Pulos
+ */
+$is_narrow = (isset($_GET['narrow'])) ? $_GET['narrow'] : 'false';
+$width = ($is_narrow == 'true') ? '185px' : '215px';
+$center_font_size = ($is_narrow == 'true') ? '11px' : '12px';
+$banner_font_size = ($is_narrow == 'true') ? '11px' : '14px';
+$footer_font_size = ($is_narrow == 'true') ? '10px' : '11px';
+$more_font_size = ($is_narrow == 'true') ? '9px' : '10px';
+$table_font_size = ($is_narrow == 'true') ? '10px' : '11px';
+
 header("Content-type: application/x-javascript");
 ?>
 document.write('<style type="text/css">\n');
 document.write('.upgotd { color: <?php echo $center_font_color ?>; font-family: Arial, Helvetica, Verdana, sans-serif; }\n');
-document.write('.upgotd-box { background-color: #EEE; border: #CCCCCC 1px dashed; text-align: center; width: 215px; font-size:12px;}\n');
-document.write('.upgotd-box a { color: <?php echo $center_link_color; ?> !important; text-decoration: none; }\n');
-document.write('.upgotd-box a:hover { color: <?php echo $center_hover_color; ?> !important; text-decoration: none; }\n');
-document.write('.upgotd-title, .upgotd-footer { padding: 3px; background-color: #BBDDFF;}\n');
-document.write('.upgotd-title, .upgotd-title a { font-weight: bold; font-size:13px !important; margin-bottom: 5px;}\n');
+document.write('.upgotd-box { background-color: <?php echo $center_bg_color; ?>; <?php echo $border; ?> text-align: center; width:<?php echo $width; ?>; font-size:12px;}\n');
+document.write('.upgotd-box a { color: <?php echo $center_link_color; ?>; text-decoration: none; }\n');
+document.write('.upgotd-box a:hover { color: <?php echo $center_hover_color; ?>; text-decoration: none; }\n');
+document.write('.upgotd-title { font-weight: bold; font-size:<?php echo $banner_font_size; ?> !important; margin-bottom: 5px; padding: 3px; background-color: <?php echo $banner_bg_color; ?>;}\n');
+document.write('.upgotd-box .upgotd-title a { font-weight: bold; font-size:<?php echo $banner_font_size; ?> !important; margin-bottom: 5px; color: <?php echo $banner_link_color; ?>;}\n');
+document.write('.upgotd-box .upgotd-title a:hover { color: <?php echo $banner_hover_color; ?>;}\n');
+document.write('.upgotd-footer { font-weight: normal ;font-size: 11px;  margin-top: 3px; padding: 3px; background-color: <?php echo $footer_bg_color; ?>; color: <?php echo $footer_font_color; ?>;}\n');
+document.write('.upgotd-box .upgotd-footer a {font-weight: normal ;font-size: <?php echo $footer_font_size; ?>;  margin-top: 3px; color: <?php echo $footer_link_color; ?>;}\n');
+document.write('.upgotd-box .upgotd-footer a:hover { color: <?php echo $footer_hover_color; ?>;}\n');
 document.write('.upgotd-image { text-align: center; }\n');
-document.write('.upgotd-pray { font-style: italic; font-weight: normal; padding: 3px 0px; font-size: 12px;}\n');
-document.write('.upgotd-people { font-weight: normal; font-size:12px !important; padding-bottom:4px; }\n');
+document.write('.upgotd-pray { font-style: italic; font-weight: normal; padding: 3px 0px; font-size: <?php echo $center_font_size; ?>;}\n');
+document.write('.upgotd-people { font-weight: normal; font-size:<?php echo $center_font_size; ?> !important; padding-bottom:4px; }\n');
 document.write('.upgotd-people a { font-weight: bold; }\n');
-document.write('.upgotd-table td { font-family: Tahoma, Verdana, Arial, Helvetica, sans-serif; font-size:11px; font-weight: normal; color: <?php echo $center_font_color ?>; line-height: 14px; text-align: left; border: 0px; background-color: #EEE; margin: 0px; padding: 0px 0px 0px 3px; }\n');
-document.write('.upgotd-more, .upgotd-more a { font-size: 10px; }\n');
-document.write('.upgotd-footer, .upgotd-footer a { font-weight: normal ;font-size: 11px;  margin-top: 3px; }\n');
+document.write('.upgotd-table td { font-family: Tahoma, Verdana, Arial, Helvetica, sans-serif; font-size:<?php echo $table_font_size; ?>; font-weight: normal; color: <?php echo $center_font_color ?>; line-height: 14px; text-align: left; border: 0px; background-color: <?php echo $center_bg_color; ?>; margin: 0px; padding: 0px 0px 0px 3px; }\n');
+document.write('.upgotd-more, .upgotd-more a { font-size: <?php echo $more_font_size; ?>; }\n');
 document.write('</style>\n');
 
-document.write('<script language="javascript" src="http://www.joshuaproject.net/upgotd_custom.php" type="text/javascript"></script>\n');
-document.write('<!-- alternative for no javascript --><noscript>\n');
-document.write('<a href="http://www.joshuaproject.net/upgotdfeed.php">View Unreached People of the Day</a></noscript>\n');
+document.write('<script language="javascript" src="<?php echo $custom_js; ?>" type="text/javascript" charset="UTF-8"></script>\n');
