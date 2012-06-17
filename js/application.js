c02bbad3136f5c59f078cf77569707dc03786505
naysayer <joshuagregoryshuster@gmail.com>
2012-06-17 01:32:57 -0700

    started to add options to make the widget update through selector options

--- js/application.js
+++ js/application.js
@@ -15,6 +15,8 @@ var bdw = '0px'; //The CSS border width.  You need to include the metric system 
 var bdc = 'FFFFFF'; //border color
 var url = 'widget.php?cfc='+ cfc +'&chc='+ chc +'&clc='+ clc +'&cbg='+ cbg +'&bbg='+ bbg +'&blc='+ blc +'&bhc='+ bhc +'&fbg='+ fbg +'&ffc='+ ffc +'&flc='+ flc +'&fhc='+ fhc +'&bdt='+ bdt +'&bdw='+ bdw +'&bdc='+ bdc +'';
 var safety_vars = ['bbg','cbg', 'cfc', 'chc', 'clc', 'blc', 'bhc', 'fbg', 'ffc', 'flc', 'fhc', 'bdt', 'bdw', 'bdc'];
+var key = '';
+var selectColor = '';
 // url = widget.php?cfc=FFFFFF&chc=898989&clc=999999&cbg=454545&bbg=098686&blc=444444&bhc=FFFFFF&fbg=767676&ffc=232323&flc=656565&fhc=898989&bdt=double&bdw=5px&bdc=FFFFFF
 $(document).ready(function() {
 	hide_border_options();
@@ -22,6 +24,8 @@ $(document).ready(function() {
 	initiate_color_picker();
 	border_options();
 	reset_all_settings();
+	added_select_boxes();
+	settings_options();
 });
 /**
 	This appends the widget to the div with a class with widget, and has it fade in.
@@ -41,6 +45,14 @@ function get_widget() {
 	;
 };
 /**
+	This function simply cleans up the code a bit, without it I would have to call the url variable
+	then the get_widget function after each and ever color changing function. 
+ */
+function update_Widget(){
+	url = 'widget.php?cfc='+ cfc +'&chc='+ chc +'&clc='+ clc +'&cbg='+ cbg +'&bbg='+ bbg +'&blc='+ blc +'&bhc='+ bhc +'&fbg='+ fbg +'&ffc='+ ffc +'&flc='+ flc +'&fhc='+ fhc +'&bdt='+ bdt +'&bdw='+ bdw +'&bdc='+ bdc +'';
+	get_widget();
+};
+/**
 	This adds the color picker to all of the input forums wiht a class of backgroud-color
 	the color swatches are technically input forums in readonly mode, with an image on top 
 	to signify that you can select colors from it. 
@@ -50,7 +62,7 @@ function initiate_color_picker(){
 		onSubmit: function(hsb, hex, rgb, el) {
 			//$(el).val(hex);
 			$(el).ColorPickerHide();
-			var key = $(el).attr('id');
+		 	key = $(el).attr('id');
 			if($.inArray(key, safety_vars) != -1) {
 				var color = hex.toUpperCase();
 				//bbg = "445566";
@@ -74,13 +86,11 @@ function initiate_color_picker(){
 function border_options(){
 	$('#select').click(function(){
 	 	bdt = $('#select').val();
-		url = 'widget.php?cfc='+ cfc +'&chc='+ chc +'&clc='+ clc +'&cbg='+ cbg +'&bbg='+ bbg +'&blc='+ blc +'&bhc='+ bhc +'&fbg='+ fbg +'&ffc='+ ffc +'&flc='+ flc +'&fhc='+ fhc +'&bdt='+ bdt +'&bdw='+ bdw +'&bdc='+ bdc +'';
-		get_widget();
+		update_Widget();
 	});
 	$('#border_width').click(function(){
 		bdw = $('#border_width').val();
-		url = 'widget.php?cfc='+ cfc +'&chc='+ chc +'&clc='+ clc +'&cbg='+ cbg +'&bbg='+ bbg +'&blc='+ blc +'&bhc='+ bhc +'&fbg='+ fbg +'&ffc='+ ffc +'&flc='+ flc +'&fhc='+ fhc +'&bdt='+ bdt +'&bdw='+ bdw +'&bdc='+ bdc +'';
-		get_widget();
+		update_Widget();
 	});
 };
 /**
@@ -103,8 +113,7 @@ function reset_all_settings(){
         bdt = 'double';//The CSS border style.  Options: dotted, dashed, solid, double, groove, ridge, inset, or outset. (default: dashed)
         bdw = '0px'; //The CSS border width.  You need to include the metric system you want to use. (default: 1px)
         bdc = 'FFFFFF'; //border color
-        url = 'widget.php?cfc='+ cfc +'&chc='+ chc +'&clc='+ clc +'&cbg='+ cbg +'&bbg='+ bbg +'&blc='+ blc +'&bhc='+ bhc +'&fbg='+ fbg +'&ffc='+ ffc +'&flc='+ flc +'&fhc='+ fhc +'&bdt='+ bdt +'&bdw='+ bdw +'&bdc='+ bdc +'';
-		get_widget();
+        update_Widget();
 	});
 };
 /**
@@ -124,9 +133,128 @@ function hide_border_options(){
 			bdt = 'double';//The CSS border style.  Options: dotted, dashed, solid, double, groove, ridge, inset, or outset. (default: dashed)
 			bdw = '0px'; //The CSS border width.  You need to include the metric system you want to use. (default: 1px)
 			bdc = 'FFFFFF'; //border color
-			url = 'widget.php?cfc='+ cfc +'&chc='+ chc +'&clc='+ clc +'&cbg='+ cbg +'&bbg='+ bbg +'&blc='+ blc +'&bhc='+ bhc +'&fbg='+ fbg +'&ffc='+ ffc +'&flc='+ flc +'&fhc='+ fhc +'&bdt='+ bdt +'&bdw='+ bdw +'&bdc='+ bdc +'';
-		get_widget();
+			update_Widget();
 	});
 };
+/**
+	This function listens in on each selector box, and changes then to them selected color.  
+ */
+function added_select_boxes(){
+	$('select.cbg').change(function(){
+		getCbgValue();
+		update_Widget();
+	});
+ 	function getCbgValue(){
+	  	cbg = $('select.cbg').val();
+	};
+	$('select.bbg').change(function(){
+		getBbgValue();
+		update_Widget();
+	});
+ 	function getBbgValue(){
+		bbg = $('select.bbg').val();
+	};
+	$('select.blc').change(function(){
+		getBlcValue();
+		update_Widget();
+	});
+ 	function getBlcValue(){
+		blc = $('select.blc').val();
+	};
+	$('select.bhc').change(function(){
+		getBhcValue();
+		update_Widget();
+	});
+ 	function getBhcValue(){
+		bhc = $('select.bhc').val();
+	};
+	$('select.cfc').change(function(){
+		getCfcValue();
+		update_Widget();
+	});
+ 	function getCfcValue(){
+		cfc = $('select.cfc').val();
+	};
+	$('select.clc').change(function(){
+		getClcValue();
+		update_Widget();
+	});
+ 	function getClcValue(){
+		clc = $('select.clc').val();
+	};
+	$('select.chc').change(function(){
+		getChcValue();
+		update_Widget();
+	});
+ 	function getChcValue(){
+		chc = $('select.chc').val();
+	};
+	$('select.fbg').change(function(){
+		getFbgValue();
+		update_Widget();
+	});
+ 	function getFbgValue(){
+		fbg = $('select.fbg').val();
+	};
+	$('select.ffc').change(function(){
+		getFfcValue();
+		update_Widget();
+	});
+ 	function getFfcValue(){
+		ffc = $('select.ffc').val();
+	};
+	$('select.flc').change(function(){
+		getFlcValue();
+		update_Widget();
+	});
+ 	function getFlcValue(){
+		flc = $('select.flc').val();
+	};
+	$('select.fhc').change(function(){
+		getFhcValue();
+		update_Widget();
+	});
+ 	function getFhcValue(){
+		fhc = $('select.fhc').val();
+	};
+};
+/**
+	On dom ready, this function hides the color picking swatches,
+	and listens for the appropriate buttons are clicked, the
+	advanced color swatches fade in, and fade out accordingly.  
+ */
+function settings_options(){
+	$('.background_color').hide();
+	$('.advanced_on').click(function(){
+		$('.background_color').fadeIn('slow');
+	});
+	$('.advanced_off').click(function(){
+		$('.background_color').fadeOut('slow');
+	});
+};
+
+
+
+// function added_select_boxes(){
+// 	
+// 	$('select').change(function(){
+// 		getSelectValue();
+// 	 	key = $(this).attr('class');
+// 		console.log(key);
+// 		console.log(selectColor);
+// 		eval(''+key+' = "'+selectColor+'"');
+// 		url = 'widget.php?cfc='+ cfc +'&chc='+ chc +'&clc='+ clc +'&cbg='+ cbg +'&bbg='+ bbg +'&blc='+ blc +'&bhc='+ bhc +'&fbg='+ fbg +'&ffc='+ ffc +'&flc='+ flc +'&fhc='+ fhc +'&bdt='+ bdt +'&bdw='+ bdw +'&bdc='+ bdc +'';
+// 		get_widget();
+// 	});
+// 	
+// 	
+//  	function getSelectValue(){
+// 	  	cbg = $('select.cbg').val();
+// 		bbg = $('select.bbg').val();
+// 		
+// 		//console.log(selectColor);	
+// 	};
+// 	
+// };
 
 
